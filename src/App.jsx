e8083e25b474bc02efe4ed6e84511c0191a45227
King-Lota <lottanna47@gmail.com>
2024-06-21 14:26:31 +0100

    Integration of Three.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-
 import { BrowserRouter } from "react-router-dom";
 
-import { Navbar, About, Contact, Experience, Feedbacks, Hero, Tech, Works, StarsCanvas } from './components';
-
+import {
+  Navbar,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
-
   return (
-   <BrowserRouter>
-   <div className="relative z-0 bg-primary">
-    <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat ">
-    <Navbar/>
-    <Hero/>
-    </div>
-    <About/>
-    <Experience/>
-    <Tech/>
-    <Works/>
-    <Feedbacks/>
-    <div className="relative z-0" >
-<Contact/>
-<StarsCanvas/>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat ">
+          <Navbar />
+          <Hero />
+        </div>
 
-    </div>
-   </div>
-   </BrowserRouter>
-  )
-}
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
