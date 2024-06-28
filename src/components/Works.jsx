b9b8e053d61b90346c754d6a5fec3587f5c10028
King-Lota <lottanna47@gmail.com>
2024-06-28 09:04:21 +0100

    Project Card Three Js Implementation

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { fadeIn, textVariant } from "../utils/motion";
+
+import { motion } from "framer-motion";
+
+const ProjectCard = (index, name , descripition, tags, image, source_code_link) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+<Tilt 
+options={{ 
+  max: 45,
+  scale: 1,
+  speed: 450
+ }}
+ className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+>
+<div className="relative w-full h-[230px]">
+<img
+src={image}
+alt={name}
+className="w-full h-full object-cover rounded-2xl"
+/>
+<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+  <div onClick={()=> window.open(source_code_link, "_blank")}></div>
+</div>
+</div>
+
+</Tilt>
+    </motion.div>
+  )
+
+}
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> My Projects</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
 
-export default Works
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", " ", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          As an experienced software developer, I have worked on a diverse range
+          of projects that showcase my ability to create innovative, efficient,
+          and user-centric solutions. Below is an overview of the types of
+          projects I have developed and contributed to, highlighting key
+          features and technologies used.
+        </motion.p>
+      </div>
+        <div className='mt-20 flex flex-wrap gap-7'>
+          {projects.map((project, index)=> (
+            <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+            />
+          ))}
+        </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
