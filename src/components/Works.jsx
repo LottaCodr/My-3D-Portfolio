f97004c1205b50455269ec0f63ea92eeb9125185
King-Lota <lottanna47@gmail.com>
2024-06-28 14:47:13 +0100

    Project Section Completed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,12 +9,12 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { motion } from "framer-motion";
 
 const ProjectCard = (
-  index,
+  {index,
   name,
-  descripition,
+  description,
   tags,
   image,
-  source_code_link
+  source_code_link}
 ) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -44,6 +44,17 @@ const ProjectCard = (
               />
             </div>
           </div>
+        </div>
+        <div className="mt-5"> 
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p></div>
+
+        <div className="mt-4 flex-wrap gap-2">
+          {tags.map((tag)=> (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
         </div>
       </Tilt>
     </motion.div>
